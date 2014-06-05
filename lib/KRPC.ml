b3d06692d2afd6efd7cdcd1cc0611472d5e5029c
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2014-06-05 14:36:50 -0400

    Prep for v0.1.3

--- lib/KRPC.ml
+++ lib/KRPC.ml
@@ -1,4 +1,4 @@
-(* Copyright (C) 2001-2013 ygrek, mldonkey
+(* Copyright (C) 2001-2014 ygrek, mldonkey
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
--- lib/kademlia.ml
+++ lib/kademlia.ml
@@ -1,4 +1,4 @@
-(* Copyright (C) 2014  Nicolas Ojeda Bar <n.oje.bar@gmail.com>
+(* Copyright (C) 2001-2014 ygrek, mldonkey
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
