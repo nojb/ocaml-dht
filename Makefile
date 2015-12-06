91fae98eb36ecf8c6aae1f57acdb34775b6f767a
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2015-12-06 10:30:57 +0100

    Fix compile

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 OCAMLFIND = ocamlfind
 LIB_DIR = lib/
 DHT_DIR = dht/
-CFLAGS = -Wall
-STDLIB_DIR = `$(OCAMLFIND) printconf stdlib`
 LIBTEST_DIR = lib_test/
+STDLIB_DIR = `$(OCAMLFIND) printconf stdlib`
+CFLAGS = -Wall
+OCAMLFLAGS = -safe-string -g
 CC = cc
 
-all: $(LIB_DIR)dht.cma $(LIBTEST_DIR)find_ih
+all: $(LIB_DIR)dht.cma $(LIBTEST_DIR)find_ih $(LIBTEST_DIR)find_ih.opt
 
 %.o: %.c
 	$(CC) $(CFLAGS) -I $(DHT_DIR) -I $(STDLIB_DIR) -I $(LIB_DIR) -o $@ -c $<
@@ -15,18 +16,31 @@ $(LIB_DIR)libdht.a: $(LIB_DIR)dhtstubs.o $(LIB_DIR)socketaddr.o $(LIB_DIR)unixsu
 	ar rvs $@ $^
 
 $(LIB_DIR)dht.cmo: $(LIB_DIR)dht.mli $(LIB_DIR)dht.ml
-	$(OCAMLFIND) ocamlc -I $(LIB_DIR) -o $@ -c $^
+	$(OCAMLFIND) ocamlc $(OCAMLFLAGS) -I $(LIB_DIR) -o $@ -c $^
 
-$(LIB_DIR)dht.cma: $(LIB_DIR)dht.cmo $(LIB_DIR)/libdht.a
-	$(OCAMLFIND) ocamlc -a -I $(LIB_DIR) -o $@ $< -cclib -ldht
+$(LIB_DIR)dht.cmx: $(LIB_DIR)dht.mli $(LIB_DIR)dht.ml
+	$(OCAMLFIND) ocamlopt $(OCAMLFLAGS) -I $(LIB_DIR) -o $@ -c $^
 
-$(LIBTEST_DIR)find_ih: $(LIB_DIR)dht.cma $(LIBTEST_DIR)find_ih.ml
-	$(OCAMLFIND) ocamlc -package lwt.unix -I $(LIB_DIR) -o $@ $<
+$(LIB_DIR)dht.cma: $(LIB_DIR)dht.cmo $(LIB_DIR)libdht.a
+	$(OCAMLFIND) ocamlc $(OCAMLFLAGS) -I $(LIB_DIR) -custom -a -o $@ $< -cclib -ldht
+
+$(LIB_DIR)dht.cmxa: $(LIB_DIR)dht.cmx $(LIB_DIR)libdht.a
+	$(OCAMLFIND) ocamlopt $(OCAMLFLAGS) -I $(LIB_DIR) -a -o $@ $< -cclib -ldht
+
+$(LIBTEST_DIR)find_ih.cmo: $(LIBTEST_DIR)find_ih.ml
+	$(OCAMLFIND) ocamlc $(OCAMLFLAGS) -package lwt.unix -I $(LIB_DIR) -linkpkg -o $@ -c $^
+
+$(LIBTEST_DIR)find_ih: $(LIBTEST_DIR)find_ih.cmo $(LIB_DIR)dht.cma
+	$(OCAMLFIND) ocamlc $(OCAMLFLAGS) -package lwt.unix -I $(LIB_DIR) -linkpkg -o $@ dht.cma $<
+
+$(LIBTEST_DIR)find_ih.opt: $(LIBTEST_DIR)find_ih.ml $(LIB_DIR)dht.cmxa
+	$(OCAMLFIND) ocamlopt $(OCAMLFLAGS) -package lwt.unix -I $(LIB_DIR) -linkpkg -o $@ dht.cmxa $<
 
 pull_jech_dht:
 	git subtree pull --prefix dht https://github.com/jech/dht master --squash
 
 clean:
-	rm -f $(LIB_DIR)*.[oa] $(LIB_DIR)*.cm[ioxa] $(LIB_DIR)*.cmxa
+	rm -f $(LIB_DIR)*.[oa] $(LIB_DIR)*.cm*
+	rm -f $(LIBTEST_DIR)find_ih.cm* $(LIBTEST_DIR)find_ih.o*
 
 .PHONY: clean pull_jech_dht
--- lib_test/find_ih.ml
+++ lib_test/find_ih.ml
@@ -20,9 +20,9 @@
    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. *)
 
 let id =
-  let s = Bytes.make 20 in
+  let s = Bytes.create 20 in
   for i = 0 to 19 do
-    Bytes.set s i (Char.of_int (Random.int 256))
+    Bytes.set s i (char_of_int (Random.int 256))
   done;
   Bytes.unsafe_to_string s
 
@@ -34,13 +34,14 @@ let string_of_sockaddr = function
   | Unix.ADDR_INET (ip, port) ->
       Printf.sprintf "%s:%d" (Unix.string_of_inet_addr ip) port
 
-let () =
+let main info_hash =
   let s = Lwt_unix.socket Unix.PF_INET Unix.SOCK_DGRAM 0 in
   let s6 = Lwt_unix.socket Unix.PF_INET6 Unix.SOCK_DGRAM 0 in
   Lwt_unix.bind s (Unix.ADDR_INET (Unix.inet_addr_any, 4567));
   Lwt_unix.bind s6 (Unix.ADDR_INET (Unix.inet_addr_any, 4568));
   Dht.init (Lwt_unix.unix_file_descr s) (Lwt_unix.unix_file_descr s6) ~id;
   let buf = Bytes.create 4096 in
+  let finished = ref false in
   let cb ev ~id =
     match ev with
     | Dht.EVENT_VALUES addrs ->
@@ -48,22 +49,74 @@ let () =
             Printf.printf "Found address: %s\n%!" (string_of_sockaddr addr)
           ) addrs
     | Dht.EVENT_SEARCH_DONE ->
-        Printf.printf "Search done.\n%!"
+        Printf.printf "Search done.\n%!";
+        finished := true
   in
   let rec loop sleep =
-    Lwt.pick
-      [
-        (Lwt_unix.sleep sleep >>= fun () -> Lwt.return `Timeout);
-        (Lwt_unix.wait_read s >>= fun () -> Lwt.return (`Read s));
-        (Lwt_unix.wait_read s6 >>= fun () -> Lwt.return (`Read s6));
-      ]
-    >>= function
-    | `Timeout ->
-        Dht.periodic None cb >>=
-        loop
-    | `Read fd ->
-        Lwt_unix.recvfrom fd buf 0 (Bytes.length buf) [] >>= fun (len, sa) ->
-        Dht.periodic (Some (buf, len, sa)) cb >>=
-        loop
+    if not !finished then
+      Lwt.pick
+        [
+          (Lwt_unix.sleep sleep >>= fun () -> Lwt.return `Timeout);
+          (Lwt_unix.wait_read s >>= fun () -> Lwt.return (`Read s));
+          (Lwt_unix.wait_read s6 >>= fun () -> Lwt.return (`Read s6));
+        ]
+      >>= function
+      | `Timeout ->
+          Lwt.wrap2 Dht.periodic None cb >>=
+          loop
+      | `Read fd ->
+          Lwt_unix.recvfrom fd buf 0 (Bytes.length buf) [] >>= fun (len, sa) ->
+          Lwt.wrap2 Dht.periodic (Some (buf, len, sa)) cb >>=
+          loop
+    else
+      Lwt.return_unit
   in
+  Dht.search ~id ~port:4567 Unix.PF_INET cb;
   loop 0.0
+
+let invalid_arg fmt =
+  Printf.ksprintf (fun str -> raise (Invalid_argument str)) fmt
+
+let hexa = "0123456789abcdef"
+and hexa1 =
+  "0000000000000000111111111111111122222222222222223333333333333333\
+   4444444444444444555555555555555566666666666666667777777777777777\
+   88888888888888889999999999999999aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbb\
+   ccccccccccccccccddddddddddddddddeeeeeeeeeeeeeeeeffffffffffffffff"
+and hexa2 =
+  "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef\
+   0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef\
+   0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef\
+   0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+
+let of_char c =
+  let x = Char.code c in
+  hexa.[x lsr 4], hexa.[x land 0xf]
+
+let to_char x y =
+  let code c = match c with
+    | '0'..'9' -> Char.code c - 48 (* Char.code '0' *)
+    | 'A'..'F' -> Char.code c - 55 (* Char.code 'A' + 10 *)
+    | 'a'..'f' -> Char.code c - 87 (* Char.code 'a' + 10 *)
+    | _ -> invalid_arg "Hex.to_char: %d is an invalid char" (Char.code c)
+  in
+  Char.chr (code x lsl 4 + code y)
+
+let to_string (`Hex (s : string)) =
+  if s = "" then ""
+  else
+    let n = String.length s in
+    let buf = Bytes.create (n/2) in
+    let rec aux i j =
+      if i >= n then ()
+      else if j >= n then invalid_arg "hex conversion: invalid hex string"
+      else (
+        Bytes.set buf (i/2) (to_char s.[i] s.[j]);
+        aux (j+1) (j+2)
+      )
+    in
+    aux 0 1;
+    Bytes.unsafe_to_string buf
+
+let () =
+  Lwt_main.run (main (to_string (`Hex Sys.argv.(1))))
