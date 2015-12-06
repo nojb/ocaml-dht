7be7073ef60e94a76a62ebef3ed8bab8715e80c2
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2015-12-06 09:36:25 +0100

    Dummy implementations of required functions

--- lib/dhtstubs.c
+++ lib/dhtstubs.c
@@ -19,6 +19,9 @@
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #include <caml/memory.h>
 #include <caml/mlvalues.h>
@@ -181,4 +184,54 @@ caml_dht_search(value id, value port, value dom, value closure)
   }
 
   CAMLreturn(Val_unit);
+}
+
+
+/* Functions called by the DHT. */
+
+int
+dht_blacklisted(const struct sockaddr *sa, int salen)
+{
+    return 0;
+}
+
+/* But for this example, we might as well use something weaker. */
+void
+dht_hash(void *hash_return, int hash_size,
+         const void *v1, int len1,
+         const void *v2, int len2,
+         const void *v3, int len3)
+{
+    const char *c1 = v1, *c2 = v2, *c3 = v3;
+    char key[9];                /* crypt is limited to 8 characters */
+    int i;
+
+    memset(key, 0, 9);
+#define CRYPT_HAPPY(c) ((c % 0x60) + 0x20)
+
+    for(i = 0; i < 2 && i < len1; i++)
+        key[i] = CRYPT_HAPPY(c1[i]);
+    for(i = 0; i < 4 && i < len1; i++)
+        key[2 + i] = CRYPT_HAPPY(c2[i]);
+    for(i = 0; i < 2 && i < len1; i++)
+        key[6 + i] = CRYPT_HAPPY(c3[i]);
+    strncpy(hash_return, crypt(key, "jc"), hash_size);
+}
+
+int
+dht_random_bytes(void *buf, size_t size)
+{
+    int fd, rc, save;
+
+    fd = open("/dev/urandom", O_RDONLY);
+    if(fd < 0)
+        return -1;
+
+    rc = read(fd, buf, size);
+
+    save = errno;
+    close(fd);
+    errno = save;
+
+    return rc;
 }
